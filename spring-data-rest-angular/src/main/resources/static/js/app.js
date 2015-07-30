da11e1dc7dab6f20a81069e9e1b35f374903b91d
markmishaev <mark.mishaev@gmail.com>
2015-07-30 17:22:40 +0300

    Add update user panel

--- spring-data-rest-angular/src/main/resources/static/js/app.js
+++ spring-data-rest-angular/src/main/resources/static/js/app.js
@@ -3,7 +3,9 @@ var userManagerModule = angular.module('userManagerApp', ['ngAnimate']);
 userManagerModule.controller('userManagerController', function ($scope,$http) {
 	
 	var urlBase="";
-	$scope.toggle=true;
+	$scope.ShowUsersList=true;
+	$scope.AddUserToggle=false;
+	$scope.UpdateUserToggle=false;
 	$scope.selection = [];
 	$http.defaults.headers.post["Content-Type"] = "application/json";
 
@@ -22,12 +24,15 @@ userManagerModule.controller('userManagerController', function ($scope,$http) {
                 $scope.firstName="";
                 $scope.lastName="";
                 $scope.isActive=false;
-                $scope.toggle='!toggle';
+                
+                $scope.AddUserToggle=false;
+                $scope.UpdateUserToggle=false;;
+                $scope.ShowUsersList=true;
             });
     }
 
     getAllUsers();
-
+		    
 	//add a new user
 	$scope.addUser = function addUser() {
 		if($scope.firstName=="" || $scope.lastName==""){
@@ -48,20 +53,46 @@ userManagerModule.controller('userManagerController', function ($scope,$http) {
 		}
 	};	
 	
-	// update user
-	   $scope.updateUser = function updateUser() {
-	          $scope.selection.forEach(function(userUri) {
-	              if (userUri != undefined) {
-	            	  console.log(userUri);
-	                  $http.patch(userUri, { 
-	                	  firstName: $scope.firstName,
-	                      lastName: $scope.lastName,
-	                      isActive: $scope.isActive});
-	              }
-	          });
-	          alert("Successfully Archived");
-	          getAllUsers();
-	   };
+   //update user
+   $scope.updateUser = function updateUser() {
+          $scope.selection.forEach(function(userUri) {
+              if (userUri != undefined) {
+            	  console.log(userUri);
+                  $http.patch(userUri, { 
+                	  firstName: $scope.firstName,
+                      lastName: $scope.lastName,
+                      isActive: $scope.isActive});
+              }
+          });
+          alert("User Updated");
+          getAllUsers();
+   };
+   
+   //ShowAddUserPanel
+   $scope.showAddUserPanel = function showAddUserPanel() 
+   {        
+		$scope.AddUserToggle=true;
+		$scope.UpdateUserToggle=false;
+		$scope.ShowUsersList = false;
+   };
+   
+ //ShowUpdateUserPanel
+   $scope.showUpdateUserPanel = function showUpdateUserPanel() 
+   {        
+
+		$scope.UpdateUserToggle=true;
+		$scope.AddUserToggle=false;
+		$scope.ShowUsersList = false;
+   };
+   
+   //ShowAllUsersPanel
+   $scope.showAllUsersPanel = function showAllUsersPanel() 
+   {        
+
+	   	$scope.ShowUsersList = true;
+		$scope.UpdateUserToggle=false;
+		$scope.AddUserToggle=false;
+   };
 	
 });
 
