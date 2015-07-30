17dfaa8ae4a13936bd4cd963ba4645fc09148905
markmishaev <mark.mishaev@gmail.com>
2015-07-30 19:10:54 +0300

    Latest Changes

--- spring-data-rest-angular/target/classes/static/js/app.js
+++ spring-data-rest-angular/target/classes/static/js/app.js
@@ -3,7 +3,10 @@ var userManagerModule = angular.module('userManagerApp', ['ngAnimate']);
 userManagerModule.controller('userManagerController', function ($scope,$http) {
 	
 	var urlBase="";
-	$scope.toggle=true;
+	$scope.ShowUsersList=true;
+	$scope.AddUserToggle=false;
+	$scope.UpdateUserToggle=false;
+	$scope.DeleteUserToggle=false;
 	$scope.selection = [];
 	$http.defaults.headers.post["Content-Type"] = "application/json";
 
@@ -16,18 +19,26 @@ userManagerModule.controller('userManagerController', function ($scope,$http) {
                 } else {
                     $scope.users = [];
                 }
-                for (var i = 0; i < $scope.users.length; i++) {
-                	$scope.selection.push($scope.users[i].id);
+                for (var i = 0; i < $scope.users.length; i++) 
+                {
+                	console.log($scope.users[i]._links.self.href);
+                	$scope.selection.push($scope.users[i]._links.self.href);
                 }
                 $scope.firstName="";
                 $scope.lastName="";
+                $scope.userId="";
                 $scope.isActive=false;
-                $scope.toggle='!toggle';
+                
+                
+                $scope.AddUserToggle=false;
+                $scope.UpdateUserToggle=false;
+                $scope.DeleteUserToggle=false;
+                $scope.ShowUsersList=true;
             });
     }
 
     getAllUsers();
-
+		    
 	//add a new user
 	$scope.addUser = function addUser() {
 		if($scope.firstName=="" || $scope.lastName==""){
@@ -48,20 +59,93 @@ userManagerModule.controller('userManagerController', function ($scope,$http) {
 		}
 	};	
 	
-	// update user
-	   $scope.updateUser = function updateUser() {
-	          $scope.selection.forEach(function(userUri) {
-	              if (userUri != undefined) {
-	            	  console.log(userUri);
-	                  $http.patch(userUri, { 
+   //update user
+   $scope.updateUser = function updateUser() {
+          $scope.selection.forEach(function(userUri) 
+		  {
+              if (userUri != undefined) 
+              {
+            	  checkedUserId = userUri.substr(userUri.lastIndexOf('/') + 1);
+            	  //console.log(userUri);
+            	  //console.log("Checked User Id: " +  checkedUserId);
+            	  //console.log("Selected User Id: " + $scope.userId);
+            	  
+            	  if(checkedUserId == $scope.userId)
+            	  {
+            		  console.log("Found the updated user." + checkedUserId + " " + $scope.userId);
+	            	  
+            		  $http.patch(userUri, { 
 	                	  firstName: $scope.firstName,
 	                      lastName: $scope.lastName,
 	                      isActive: $scope.isActive});
-	              }
-	          });
-	          alert("Successfully Archived");
-	          getAllUsers();
-	   };
+            	  }
+              }
+          });
+          alert("User Updated");
+          getAllUsers();
+   };
+   
+   //delete user
+   $scope.deleteUser = function deleteUser() {
+          $scope.selection.forEach(function(userUri) 
+		  {
+              if (userUri != undefined) 
+              {
+            	  checkedUserId = userUri.substr(userUri.lastIndexOf('/') + 1);
+            	  
+            	  if(checkedUserId == $scope.userId)
+            	  {
+            		  var idx = $scope.selection.indexOf(userUri);
+            		  $scope.selection.splice(idx, 1);
+            		  
+            		  console.log("Found the deleted user." + checkedUserId + " " + $scope.userId);
+	            	  
+            		  $http.delete(userUri);
+            	  }
+              }
+          });
+          alert("User Deleted");
+          getAllUsers();
+   };
+   
+   
+   //ShowAddUserPanel
+   $scope.showAddUserPanel = function showAddUserPanel() 
+   {        
+		$scope.AddUserToggle=true;
+		$scope.UpdateUserToggle=false;
+		$scope.DeleteUserToggle=false;
+		$scope.ShowUsersList = false;
+   };
+   
+ //ShowUpdateUserPanel
+   $scope.showUpdateUserPanel = function showUpdateUserPanel() 
+   {        
+
+		$scope.UpdateUserToggle=true;
+		$scope.AddUserToggle=false;
+		$scope.DeleteUserToggle=false;
+		$scope.ShowUsersList = false;
+   };
+   
+ //ShowDeleteUserPanel
+   $scope.showDeleteUserPanel = function showDeleteUserPanel() 
+   {        
+	   $scope.DeleteUserToggle=true;
+		$scope.UpdateUserToggle=false;
+		$scope.AddUserToggle=false;
+		$scope.ShowUsersList = false;
+   };
+   
+   //ShowAllUsersPanel
+   $scope.showAllUsersPanel = function showAllUsersPanel() 
+   {        
+
+	   	$scope.ShowUsersList = true;
+		$scope.UpdateUserToggle=false;
+		$scope.DeleteUserToggle=false;
+		$scope.AddUserToggle=false;
+   };
 	
 });
 
